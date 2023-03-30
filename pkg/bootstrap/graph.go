ed163e3aba4ea017f9be03751cc5089cd49920ad
Srevin Saju <srevinsaju@sugarlabs.org>
2023-03-30 10:31:12 +0530

    feat: add regex support for dependencies

--- pkg/bootstrap/graph.go
+++ pkg/bootstrap/graph.go
@@ -1,9 +1,12 @@
 package bootstrap
 
 import (
+	"fmt"
 	"github.com/kendru/darwin/go/depgraph"
 	"github.com/srevinsaju/togomak/pkg/context"
 	"github.com/srevinsaju/togomak/pkg/schema"
+	"regexp"
+	"strings"
 )
 
 func Graph(ctx *context.Context, data schema.SchemaConfig) {
@@ -22,10 +25,49 @@ func Graph(ctx *context.Context, data schema.SchemaConfig) {
 			}
 		}
 		for _, dep := range stage.DependsOn {
-			graphLog.Debugf("%s stage depends on %s stage", dep, stage.Id)
-			err := graph.DependOn(stage.Id, dep)
-			if err != nil {
-				ctx.Logger.Fatalf("Error while creating the dependency tree for '%s -> %s': %s", stage.Id, dep, err)
+			graphLog.Debugf("r'%s' stage depends on %s stage", dep, stage.Id)
+			found := false
+
+			if strings.HasPrefix(dep, "/") && strings.HasSuffix(dep, "/") {
+
+				for _, s := range data.Stages {
+					v, err := regexp.MatchString(fmt.Sprintf("%s$", strings.Trim(dep, "/")), s.Id)
+					if err != nil {
+						panic(err)
+					}
+					if v {
+						found = true
+						// the stage exists
+						graphLog.Debugf("%s stage depends on %s stage", s.Id, stage.Id)
+						if s.Id == stage.Id {
+							// skip if the stage depends on itself
+							continue
+						}
+						err := graph.DependOn(stage.Id, s.Id)
+						if err != nil {
+							ctx.Logger.Fatalf("Error while creating the dependency tree for '%s -> %s': %s", stage.Id, s.Id, err)
+						}
+					}
+				}
+			} else {
+				for _, s := range data.Stages {
+					if s.Id == dep {
+						found = true
+						// the stage exists
+						graphLog.Debugf("%s stage depends on %s stage", s.Id, stage.Id)
+						if s.Id == stage.Id {
+							// skip if the stage depends on itself
+							continue
+						}
+						err := graph.DependOn(stage.Id, s.Id)
+						if err != nil {
+							ctx.Logger.Fatalf("Error while creating the dependency tree for '%s -> %s': %s", stage.Id, s.Id, err)
+						}
+					}
+				}
+			}
+			if !found {
+				ctx.Logger.Fatalf("Error while creating the dependency tree for '%s -> %s': %s", stage.Id, dep, "stage not found")
 			}
 
 		}
