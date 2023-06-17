srevinsaju/togomak

commit f925124b1b2f3d7dac9aba78ef21558b8ed619bf

feat(orchestra): refactor common functions

pkg/ci/stage_run.go
@@ -13,6 +13,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/srevinsaju/togomak/v1/pkg/c"
 	"github.com/srevinsaju/togomak/v1/pkg/diag"
+	"github.com/srevinsaju/togomak/v1/pkg/meta"
 	"github.com/srevinsaju/togomak/v1/pkg/ui"
 	"github.com/zclconf/go-cty/cty"
 	"io"
@@ -57,7 +58,7 @@ func (s *Stage) expandMacros(ctx context.Context) (*Stage, hcl.Diagnostics) {
 	pipe := ctx.Value(c.TogomakContextPipeline).(*Pipeline)
 	cwd := ctx.Value(c.TogomakContextCwd).(string)
 	tmpDir := ctx.Value(c.TogomakContextTempDir).(string)
-	logger.Debugf("running %s.%s.%s", StageBlock, s.Id, MacroBlock)
+	logger.Debugf("running %s.%s", s.Identifier(), MacroBlock)
 
 	var hclDiags hcl.Diagnostics
 	var err error
@@ -145,7 +146,7 @@ func (s *Stage) expandMacros(ctx context.Context) (*Stage, hcl.Diagnostics) {
 
 			for fName, fContent := range files {
 				lastExecutionPath = filepath.Join(tmpDir, s.Id, fName)
-				if filepath.Base(fName) == "togomak.hcl" {
+				if filepath.Base(fName) == meta.ConfigFileName {
 					defaultExecutionPath = filepath.Join(tmpDir, s.Id, fName)
 				}
 				// write the file content to the temporary directory

pkg/orchestra/context.go
@@ -1,0 +1,260 @@
+package orchestra
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/ext/tryfunc"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/sirupsen/logrus"
+	"github.com/srevinsaju/togomak/v1/pkg/c"
+	"github.com/srevinsaju/togomak/v1/pkg/meta"
+	"github.com/srevinsaju/togomak/v1/pkg/third-party/hashicorp/terraform/lang/funcs"
+	"github.com/srevinsaju/togomak/v1/pkg/x"
+	ctyyaml "github.com/zclconf/go-cty-yaml"
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/function"
+	"github.com/zclconf/go-cty/cty/function/stdlib"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+type Togomak struct {
+	logger        *logrus.Logger
+	pipelineId    string
+	cfg           Config
+	cwd           string
+	ectx          *hcl.EvalContext
+	tempDir       string
+	parser        *hclparse.Parser
+	hclDiagWriter hcl.DiagnosticWriter
+}
+
+func (t Togomak) Logger() *logrus.Logger {
+	return t.logger
+}
+
+func (t Togomak) Parser() *hclparse.Parser {
+	return t.parser
+}
+
+func NewContextWithTogomak(cfg Config) (Togomak, context.Context) {
+
+	logger := NewLogger(cfg)
+	if !cfg.Child {
+		logger.Infof("%s (version=%s)", meta.AppName, meta.AppVersion)
+	}
+
+	// create temporary directory
+	pipelineId := uuid.New().String()
+	tmpDir := filepath.Join(meta.BuildDirPrefix, "pipelines", "tmp")
+	err := os.MkdirAll(tmpDir, 0755)
+	x.Must(err)
+	tmpDir, err = os.MkdirTemp(tmpDir, pipelineId)
+	x.Must(err)
+	// --> set up the working directory
+	cwd := Chdir(cfg, logger)
+	x.Must(err)
+	fmt.Println(cwd)
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, c.TogomakContextTempDir, tmpDir)
+	ctx = context.WithValue(ctx, c.TogomakContextLogger, logger)
+	ctx = context.WithValue(ctx, c.TogomakContextBootTime, time.Now())
+	ctx = context.WithValue(ctx, c.TogomakContextPipelineId, pipelineId)
+	ctx = context.WithValue(ctx, c.TogomakContextOwd, cfg.Owd)
+	ctx = context.WithValue(ctx, c.TogomakContextCwd, cwd)
+	ctx = context.WithValue(ctx, c.TogomakContextHostname, cfg.Hostname)
+	ctx = context.WithValue(ctx, c.TogomakContextUsername, cfg.User)
+	ctx = context.WithValue(ctx, c.TogomakContextPipelineFilePath, cfg.Pipeline.FilePath)
+	ctx = context.WithValue(ctx, c.TogomakContextPipelineDryRun, cfg.Pipeline.DryRun)
+	ctx = context.WithValue(ctx, c.TogomakContextPipelineTmpDir, tmpDir)
+
+	ctx = context.WithValue(ctx, c.TogomakContextMutexStages, &sync.Mutex{})
+	ctx = context.WithValue(ctx, c.TogomakContextMutexData, &sync.Mutex{})
+	ctx = context.WithValue(ctx, c.TogomakContextMutexLocals, &sync.Mutex{})
+	ctx = context.WithValue(ctx, c.TogomakContextMutexMacro, &sync.Mutex{})
+
+	// --> set up HCL context
+	hclContext := &hcl.EvalContext{
+		Functions: map[string]function.Function{
+			"abs":              stdlib.AbsoluteFunc,
+			"abspath":          funcs.AbsPathFunc,
+			"alltrue":          funcs.AllTrueFunc,
+			"anytrue":          funcs.AnyTrueFunc,
+			"basename":         funcs.BasenameFunc,
+			"base64decode":     funcs.Base64DecodeFunc,
+			"base64encode":     funcs.Base64EncodeFunc,
+			"base64gzip":       funcs.Base64GzipFunc,
+			"base64sha256":     funcs.Base64Sha256Func,
+			"base64sha512":     funcs.Base64Sha512Func,
+			"bcrypt":           funcs.BcryptFunc,
+			"can":              tryfunc.CanFunc,
+			"ceil":             stdlib.CeilFunc,
+			"chomp":            stdlib.ChompFunc,
+			"coalesce":         funcs.CoalesceFunc,
+			"coalescelist":     stdlib.CoalesceListFunc,
+			"compact":          stdlib.CompactFunc,
+			"concat":           stdlib.ConcatFunc,
+			"contains":         stdlib.ContainsFunc,
+			"csvdecode":        stdlib.CSVDecodeFunc,
+			"dirname":          funcs.DirnameFunc,
+			"distinct":         stdlib.DistinctFunc,
+			"element":          stdlib.ElementFunc,
+			"endswith":         funcs.EndsWithFunc,
+			"chunklist":        stdlib.ChunklistFunc,
+			"file":             funcs.MakeFileFunc(cwd, false),
+			"fileexists":       funcs.MakeFileExistsFunc(cwd),
+			"fileset":          funcs.MakeFileSetFunc(cwd),
+			"filebase64":       funcs.MakeFileFunc(cwd, true),
+			"filebase64sha256": funcs.MakeFileBase64Sha256Func(cwd),
+			"filebase64sha512": funcs.MakeFileBase64Sha512Func(cwd),
+			"filemd5":          funcs.MakeFileMd5Func(cwd),
+			"filesha1":         funcs.MakeFileSha1Func(cwd),
+			"filesha256":       funcs.MakeFileSha256Func(cwd),
+			"filesha512":       funcs.MakeFileSha512Func(cwd),
+			"flatten":          stdlib.FlattenFunc,
+			"floor":            stdlib.FloorFunc,
+			"format":           stdlib.FormatFunc,
+			"formatdate":       stdlib.FormatDateFunc,
+			"formatlist":       stdlib.FormatListFunc,
+			"indent":           stdlib.IndentFunc,
+			"index":            funcs.IndexFunc, // stdlib.IndexFunc is not compatible
+			"join":             stdlib.JoinFunc,
+			"jsondecode":       stdlib.JSONDecodeFunc,
+			"jsonencode":       stdlib.JSONEncodeFunc,
+			"keys":             stdlib.KeysFunc,
+			"length":           funcs.LengthFunc,
+			"list":             funcs.ListFunc,
+			"log":              stdlib.LogFunc,
+			"lookup":           funcs.LookupFunc,
+			"lower":            stdlib.LowerFunc,
+			"map":              funcs.MapFunc,
+			"matchkeys":        funcs.MatchkeysFunc,
+			"max":              stdlib.MaxFunc,
+			"md5":              funcs.Md5Func,
+			"merge":            stdlib.MergeFunc,
+			"min":              stdlib.MinFunc,
+			"one":              funcs.OneFunc,
+			"parseint":         stdlib.ParseIntFunc,
+			"pathexpand":       funcs.PathExpandFunc,
+			"pow":              stdlib.PowFunc,
+			"range":            stdlib.RangeFunc,
+			"regex":            stdlib.RegexFunc,
+			"regexall":         stdlib.RegexAllFunc,
+			"replace":          funcs.ReplaceFunc,
+			"reverse":          stdlib.ReverseListFunc,
+			"rsadecrypt":       funcs.RsaDecryptFunc,
+			"sensitive":        funcs.SensitiveFunc,
+			"nonsensitive":     funcs.NonsensitiveFunc,
+			"setintersection":  stdlib.SetIntersectionFunc,
+			"setproduct":       stdlib.SetProductFunc,
+			"setsubtract":      stdlib.SetSubtractFunc,
+			"setunion":         stdlib.SetUnionFunc,
+			"sha1":             funcs.Sha1Func,
+			"sha256":           funcs.Sha256Func,
+			"sha512":           funcs.Sha512Func,
+			"signum":           stdlib.SignumFunc,
+			"slice":            stdlib.SliceFunc,
+			"sort":             stdlib.SortFunc,
+			"split":            stdlib.SplitFunc,
+			"startswith":       funcs.StartsWithFunc,
+			"strcontains":      funcs.StrContainsFunc,
+			"strrev":           stdlib.ReverseFunc,
+			"substr":           stdlib.SubstrFunc,
+			"sum":              funcs.SumFunc,
+			"textdecodebase64": funcs.TextDecodeBase64Func,
+			"textencodebase64": funcs.TextEncodeBase64Func,
+			"timestamp":        funcs.TimestampFunc,
+			"timeadd":          stdlib.TimeAddFunc,
+			"timecmp":          funcs.TimeCmpFunc,
+			"title":            stdlib.TitleFunc,
+			"tostring":         funcs.MakeToFunc(cty.String),
+			"tonumber":         funcs.MakeToFunc(cty.Number),
+			"tobool":           funcs.MakeToFunc(cty.Bool),
+			"toset":            funcs.MakeToFunc(cty.Set(cty.DynamicPseudoType)),
+			"tolist":           funcs.MakeToFunc(cty.List(cty.DynamicPseudoType)),
+			"tomap":            funcs.MakeToFunc(cty.Map(cty.DynamicPseudoType)),
+			"transpose":        funcs.TransposeFunc,
+			"trim":             stdlib.TrimFunc,
+			"trimprefix":       stdlib.TrimPrefixFunc,
+			"trimspace":        stdlib.TrimSpaceFunc,
+			"trimsuffix":       stdlib.TrimSuffixFunc,
+			"try":              tryfunc.TryFunc,
+			"upper":            stdlib.UpperFunc,
+			"urlencode":        funcs.URLEncodeFunc,
+			"uuid":             funcs.UUIDFunc,
+			"uuidv5":           funcs.UUIDV5Func,
+			"values":           stdlib.ValuesFunc,
+			"which": function.New(&function.Spec{
+				Params: []function.Parameter{
+					{
+						Name:             "executable",
+						AllowDynamicType: true,
+						Type:             cty.String,
+					},
+				},
+				Type: function.StaticReturnType(cty.String),
+				Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+					path, err := exec.LookPath(args[0].AsString())
+					if err != nil {
+						return cty.StringVal(""), err
+					}
+					return cty.StringVal(path), nil
+				},
+				Description: "Returns the absolute path to an executable in the current PATH.",
+			}),
+			"yamldecode": ctyyaml.YAMLDecodeFunc,
+			"yamlencode": ctyyaml.YAMLEncodeFunc,
+			"zipmap":     stdlib.ZipmapFunc,
+		},
+
+		Variables: map[string]cty.Value{
+			"true":  cty.True,
+			"false": cty.False,
+			"null":  cty.NullVal(cty.DynamicPseudoType),
+
+			c.TogomakContextOwd:      cty.StringVal(cfg.Owd),
+			c.TogomakContextCwd:      cty.StringVal(cwd),
+			c.TogomakContextHostname: cty.StringVal(cfg.Hostname),
+			c.TogomakContextUsername: cty.StringVal(cfg.User),
+
+			"pipeline": cty.ObjectVal(map[string]cty.Value{
+				"id":     cty.StringVal(pipelineId),
+				"path":   cty.StringVal(cfg.Pipeline.FilePath),
+				"tmpDir": cty.StringVal(tmpDir),
+			}),
+
+			"togomak": cty.ObjectVal(map[string]cty.Value{
+				"version":        cty.StringVal(meta.AppVersion),
+				"boot_time":      cty.StringVal(time.Now().Format(time.RFC3339)),
+				"boot_time_unix": cty.NumberIntVal(time.Now().Unix()),
+				"pipeline_id":    cty.StringVal(pipelineId),
+				"ci":             cty.BoolVal(cfg.Ci),
+				"unattended":     cty.BoolVal(cfg.Unattended),
+			}),
+		},
+	}
+
+	parser := hclparse.NewParser()
+	diagnosticTextWriter := hcl.NewDiagnosticTextWriter(logger.Writer(), parser.Files(), 0, true)
+	ctx = context.WithValue(ctx, c.TogomakContextHclDiagWriter, diagnosticTextWriter)
+
+	ctx = context.WithValue(ctx, c.TogomakContextHclEval, hclContext)
+	t := Togomak{
+		logger:        logger,
+		pipelineId:    pipelineId,
+		cfg:           cfg,
+		cwd:           cwd,
+		hclDiagWriter: diagnosticTextWriter,
+		parser:        parser,
+		ectx:          hclContext,
+		tempDir:       "",
+	}
+
+	return t, ctx
+}

examples/files/togomak.hcl
@@ -1,0 +1,9 @@
+togomak {
+  version = 1
+}
+
+
+stage "list" {
+  name   = "listing files"
+  args = ["ls", "-al"]
+}

pkg/c/context_test.go
@@ -1,0 +1,7 @@
+package c_test
+
+import "testing"
+
+func TestContext(t *testing.T) {
+	return
+}

pkg/errors/diagnostics.go
@@ -1,1 +1,0 @@
-package errors

pkg/orchestra/orchestra.go
@@ -3,10 +3,6 @@ package orchestra
 import (
 	"context"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/ext/tryfunc"
-	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/kendru/darwin/go/depgraph"
 	"github.com/sirupsen/logrus"
 	"github.com/srevinsaju/togomak/v1/pkg/c"
@@ -15,15 +11,11 @@ import (
 	"github.com/srevinsaju/togomak/v1/pkg/graph"
 	"github.com/srevinsaju/togomak/v1/pkg/meta"
 	"github.com/srevinsaju/togomak/v1/pkg/pipeline"
-	"github.com/srevinsaju/togomak/v1/pkg/third-party/hashicorp/terraform/lang/funcs"
 	"github.com/srevinsaju/togomak/v1/pkg/ui"
 	"github.com/srevinsaju/togomak/v1/pkg/x"
-	ctyyaml "github.com/zclconf/go-cty-yaml"
+
 	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/function"
-	"github.com/zclconf/go-cty/cty/function/stdlib"
 	"os"
-	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"strings"
@@ -75,51 +67,21 @@ func Chdir(cfg Config, logger *logrus.Logger) string {
 	if err != nil {
 		logger.Fatal(err)
 	}
+	cwd, err = os.Getwd()
+	x.Must(err)
 	return cwd
+
 }
 
 func Orchestra(cfg Config) {
-	// --> set up the logger
-	logger := NewLogger(cfg)
-	if !cfg.Child {
-		logger.Infof("%s (version=%s)", meta.AppName, meta.AppVersion)
-	}
-
-	// --> set up the working directory
-	cwd := Chdir(cfg, logger)
-
-	// --> set up the context
-	// we will now create a long-running background context
-	// and gather necessary data before reading the pipeline
-	pipelineId := uuid.New().String()
-	tmpDir := filepath.Join(meta.BuildDirPrefix, "pipelines", "tmp")
-	err := os.MkdirAll(tmpDir, 0755)
-	x.Must(err)
-	tmpDir, err = os.MkdirTemp(tmpDir, pipelineId)
-	x.Must(err)
-	ctx := context.Background()
+	var diags diag.Diagnostics
+	t, ctx := NewContextWithTogomak(cfg)
 	ctx, cancel := context.WithCancel(ctx)
-	ctx = context.WithValue(ctx, c.TogomakContextTempDir, tmpDir)
-	ctx = context.WithValue(ctx, c.TogomakContextLogger, logger)
-	ctx = context.WithValue(ctx, c.TogomakContextBootTime, time.Now())
-	ctx = context.WithValue(ctx, c.TogomakContextPipelineId, pipelineId)
-	ctx = context.WithValue(ctx, c.TogomakContextOwd, cfg.Owd)
-	ctx = context.WithValue(ctx, c.TogomakContextCwd, cwd)
-	ctx = context.WithValue(ctx, c.TogomakContextHostname, cfg.Hostname)
-	ctx = context.WithValue(ctx, c.TogomakContextUsername, cfg.User)
-	ctx = context.WithValue(ctx, c.TogomakContextPipelineFilePath, cfg.Pipeline.FilePath)
-	ctx = context.WithValue(ctx, c.TogomakContextPipelineDryRun, cfg.Pipeline.DryRun)
-	ctx = context.WithValue(ctx, c.TogomakContextPipelineTmpDir, tmpDir)
-
-	ctx = context.WithValue(ctx, c.TogomakContextMutexStages, &sync.Mutex{})
-	ctx = context.WithValue(ctx, c.TogomakContextMutexData, &sync.Mutex{})
-	ctx = context.WithValue(ctx, c.TogomakContextMutexLocals, &sync.Mutex{})
-	ctx = context.WithValue(ctx, c.TogomakContextMutexMacro, &sync.Mutex{})
+	logger := t.logger
 
 	// region: external parameters
 	paramsGo := make(map[string]cty.Value)
 	if cfg.Child {
-
 		m := make(map[string]string)
 		for _, e := range os.Environ() {
 			if i := strings.Index(e, "="); i >= 0 {
@@ -134,192 +96,26 @@ func Orchestra(cfg Config) {
 			}
 		}
 	}
-
-	// --> set up HCL context
-	hclContext := &hcl.EvalContext{
-		Functions: map[string]function.Function{
-			"abs":              stdlib.AbsoluteFunc,
-			"abspath":          funcs.AbsPathFunc,
-			"alltrue":          funcs.AllTrueFunc,
-			"anytrue":          funcs.AnyTrueFunc,
-			"basename":         funcs.BasenameFunc,
-			"base64decode":     funcs.Base64DecodeFunc,
-			"base64encode":     funcs.Base64EncodeFunc,
-			"base64gzip":       funcs.Base64GzipFunc,
-			"base64sha256":     funcs.Base64Sha256Func,
-			"base64sha512":     funcs.Base64Sha512Func,
-			"bcrypt":           funcs.BcryptFunc,
-			"can":              tryfunc.CanFunc,
-			"ceil":             stdlib.CeilFunc,
-			"chomp":            stdlib.ChompFunc,
-			"coalesce":         funcs.CoalesceFunc,
-			"coalescelist":     stdlib.CoalesceListFunc,
-			"compact":          stdlib.CompactFunc,
-			"concat":           stdlib.ConcatFunc,
-			"contains":         stdlib.ContainsFunc,
-			"csvdecode":        stdlib.CSVDecodeFunc,
-			"dirname":          funcs.DirnameFunc,
-			"distinct":         stdlib.DistinctFunc,
-			"element":          stdlib.ElementFunc,
-			"endswith":         funcs.EndsWithFunc,
-			"chunklist":        stdlib.ChunklistFunc,
-			"file":             funcs.MakeFileFunc(cwd, false),
-			"fileexists":       funcs.MakeFileExistsFunc(cwd),
-			"fileset":          funcs.MakeFileSetFunc(cwd),
-			"filebase64":       funcs.MakeFileFunc(cwd, true),
-			"filebase64sha256": funcs.MakeFileBase64Sha256Func(cwd),
-			"filebase64sha512": funcs.MakeFileBase64Sha512Func(cwd),
-			"filemd5":          funcs.MakeFileMd5Func(cwd),
-			"filesha1":         funcs.MakeFileSha1Func(cwd),
-			"filesha256":       funcs.MakeFileSha256Func(cwd),
-			"filesha512":       funcs.MakeFileSha512Func(cwd),
-			"flatten":          stdlib.FlattenFunc,
-			"floor":            stdlib.FloorFunc,
-			"format":           stdlib.FormatFunc,
-			"formatdate":       stdlib.FormatDateFunc,
-			"formatlist":       stdlib.FormatListFunc,
-			"indent":           stdlib.IndentFunc,
-			"index":            funcs.IndexFunc, // stdlib.IndexFunc is not compatible
-			"join":             stdlib.JoinFunc,
-			"jsondecode":       stdlib.JSONDecodeFunc,
-			"jsonencode":       stdlib.JSONEncodeFunc,
-			"keys":             stdlib.KeysFunc,
-			"length":           funcs.LengthFunc,
-			"list":             funcs.ListFunc,
-			"log":              stdlib.LogFunc,
-			"lookup":           funcs.LookupFunc,
-			"lower":            stdlib.LowerFunc,
-			"map":              funcs.MapFunc,
-			"matchkeys":        funcs.MatchkeysFunc,
-			"max":              stdlib.MaxFunc,
-			"md5":              funcs.Md5Func,
-			"merge":            stdlib.MergeFunc,
-			"min":              stdlib.MinFunc,
-			"one":              funcs.OneFunc,
-			"parseint":         stdlib.ParseIntFunc,
-			"pathexpand":       funcs.PathExpandFunc,
-			"pow":              stdlib.PowFunc,
-			"range":            stdlib.RangeFunc,
-			"regex":            stdlib.RegexFunc,
-			"regexall":         stdlib.RegexAllFunc,
-			"replace":          funcs.ReplaceFunc,
-			"reverse":          stdlib.ReverseListFunc,
-			"rsadecrypt":       funcs.RsaDecryptFunc,
-			"sensitive":        funcs.SensitiveFunc,
-			"nonsensitive":     funcs.NonsensitiveFunc,
-			"setintersection":  stdlib.SetIntersectionFunc,
-			"setproduct":       stdlib.SetProductFunc,
-			"setsubtract":      stdlib.SetSubtractFunc,
-			"setunion":         stdlib.SetUnionFunc,
-			"sha1":             funcs.Sha1Func,
-			"sha256":           funcs.Sha256Func,
-			"sha512":           funcs.Sha512Func,
-			"signum":           stdlib.SignumFunc,
-			"slice":            stdlib.SliceFunc,
-			"sort":             stdlib.SortFunc,
-			"split":            stdlib.SplitFunc,
-			"startswith":       funcs.StartsWithFunc,
-			"strcontains":      funcs.StrContainsFunc,
-			"strrev":           stdlib.ReverseFunc,
-			"substr":           stdlib.SubstrFunc,
-			"sum":              funcs.SumFunc,
-			"textdecodebase64": funcs.TextDecodeBase64Func,
-			"textencodebase64": funcs.TextEncodeBase64Func,
-			"timestamp":        funcs.TimestampFunc,
-			"timeadd":          stdlib.TimeAddFunc,
-			"timecmp":          funcs.TimeCmpFunc,
-			"title":            stdlib.TitleFunc,
-			"tostring":         funcs.MakeToFunc(cty.String),
-			"tonumber":         funcs.MakeToFunc(cty.Number),
-			"tobool":           funcs.MakeToFunc(cty.Bool),
-			"toset":            funcs.MakeToFunc(cty.Set(cty.DynamicPseudoType)),
-			"tolist":           funcs.MakeToFunc(cty.List(cty.DynamicPseudoType)),
-			"tomap":            funcs.MakeToFunc(cty.Map(cty.DynamicPseudoType)),
-			"transpose":        funcs.TransposeFunc,
-			"trim":             stdlib.TrimFunc,
-			"trimprefix":       stdlib.TrimPrefixFunc,
-			"trimspace":        stdlib.TrimSpaceFunc,
-			"trimsuffix":       stdlib.TrimSuffixFunc,
-			"try":              tryfunc.TryFunc,
-			"upper":            stdlib.UpperFunc,
-			"urlencode":        funcs.URLEncodeFunc,
-			"uuid":             funcs.UUIDFunc,
-			"uuidv5":           funcs.UUIDV5Func,
-			"values":           stdlib.ValuesFunc,
-			"which": function.New(&function.Spec{
-				Params: []function.Parameter{
-					{
-						Name:             "executable",
-						AllowDynamicType: true,
-						Type:             cty.String,
-					},
-				},
-				Type: function.StaticReturnType(cty.String),
-				Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-					path, err := exec.LookPath(args[0].AsString())
-					if err != nil {
-						return cty.StringVal(""), err
-					}
-					return cty.StringVal(path), nil
-				},
-				Description: "Returns the absolute path to an executable in the current PATH.",
-			}),
-			"yamldecode": ctyyaml.YAMLDecodeFunc,
-			"yamlencode": ctyyaml.YAMLEncodeFunc,
-			"zipmap":     stdlib.ZipmapFunc,
-		},
-
-		Variables: map[string]cty.Value{
-			"true":  cty.True,
-			"false": cty.False,
-			"null":  cty.NullVal(cty.DynamicPseudoType),
-
-			c.TogomakContextOwd:      cty.StringVal(cfg.Owd),
-			c.TogomakContextCwd:      cty.StringVal(cwd),
-			c.TogomakContextHostname: cty.StringVal(cfg.Hostname),
-			c.TogomakContextUsername: cty.StringVal(cfg.User),
-
-			"pipeline": cty.ObjectVal(map[string]cty.Value{
-				"id":     cty.StringVal(pipelineId),
-				"path":   cty.StringVal(cfg.Pipeline.FilePath),
-				"tmpDir": cty.StringVal(tmpDir),
-			}),
-
-			"param": cty.ObjectVal(paramsGo),
-
-			"togomak": cty.ObjectVal(map[string]cty.Value{
-				"version":        cty.StringVal(meta.AppVersion),
-				"boot_time":      cty.StringVal(time.Now().Format(time.RFC3339)),
-				"boot_time_unix": cty.NumberIntVal(time.Now().Unix()),
-				"pipeline_id":    cty.StringVal(pipelineId),
-				"ci":             cty.BoolVal(cfg.Ci),
-				"unattended":     cty.BoolVal(cfg.Unattended),
-			}),
-		},
-	}
-	ctx = context.WithValue(ctx, c.TogomakContextHclEval, hclContext)
+	t.ectx.Variables["param"] = cty.ObjectVal(paramsGo)
+	// endregion
 
 	// --> parse the config file
 	// we will now read the pipeline from togomak.hcl
-	parser := hclparse.NewParser()
-	var diags diag.Diagnostics
-	dgwriter := hcl.NewDiagnosticTextWriter(logger.Writer(), parser.Files(), 0, true)
-	ctx = context.WithValue(ctx, c.TogomakContextHclDiagWriter, dgwriter)
-	pipe, hclDiags := pipeline.Read(ctx, parser)
+	pipe, hclDiags := pipeline.Read(ctx, t.parser)
 	if hclDiags.HasErrors() {
-		logger.Fatal(dgwriter.WriteDiagnostics(hclDiags))
+		logger.Fatal(t.hclDiagWriter.WriteDiagnostics(hclDiags))
 	}
 
 	// whitelist all stages if unspecified
-	var stageStatuses = cfg.Pipeline.Stages
+	stageStatuses := cfg.Pipeline.Stages
 
 	// write the pipeline to the temporary directory
-	pipelineFilePath := filepath.Join(tmpDir, meta.ConfigFileName)
-	pipelineData := []byte{}
-	for _, f := range parser.Files() {
+	pipelineFilePath := filepath.Join(t.tempDir, meta.ConfigFileName)
+	var pipelineData []byte
+	for _, f := range t.parser.Files() {
 		pipelineData = append(pipelineData, f.Bytes...)
 	}
-	err = os.WriteFile(pipelineFilePath, pipelineData, 0644)
+	err := os.WriteFile(pipelineFilePath, pipelineData, 0644)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -343,7 +139,6 @@ func Orchestra(cfg Config) {
 	var depGraph *depgraph.Graph
 	depGraph, diags = graph.TopoSort(ctx, pipe)
 	if diags.HasErrors() {
-
 		diags.Fatal(logger.WriterLevel(logrus.ErrorLevel))
 	}
 
@@ -354,7 +149,6 @@ func Orchestra(cfg Config) {
 	var wg sync.WaitGroup
 	var daemonWg sync.WaitGroup
 	var hasDaemons bool
-
 	var runnables ci.Runnables
 
 	// region: interrupt handler

pkg/third-party/hashicorp/terraform/lang/funcs/collection.go
@@ -35,6 +35,7 @@ var LengthFunc = function.New(&function.Spec{
 			return cty.Number, errors.New("argument must be a string, a collection type, or a structural type")
 		}
 	},
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
 		coll := args[0]
 		collTy := args[0].Type()
@@ -71,7 +72,8 @@ var AllTrueFunc = function.New(&function.Spec{
 			Type: cty.List(cty.Bool),
 		},
 	},
-	Type: function.StaticReturnType(cty.Bool),
+	Type:         function.StaticReturnType(cty.Bool),
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		result := cty.True
 		for it := args[0].ElementIterator(); it.Next(); {
@@ -100,7 +102,8 @@ var AnyTrueFunc = function.New(&function.Spec{
 			Type: cty.List(cty.Bool),
 		},
 	},
-	Type: function.StaticReturnType(cty.Bool),
+	Type:         function.StaticReturnType(cty.Bool),
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		result := cty.False
 		var hasUnknown bool
@@ -149,6 +152,7 @@ var CoalesceFunc = function.New(&function.Spec{
 		}
 		return retType, nil
 	},
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		for _, argVal := range args {
 			// We already know this will succeed because of the checks in our Type func above
@@ -181,7 +185,8 @@ var IndexFunc = function.New(&function.Spec{
 			Type: cty.DynamicPseudoType,
 		},
 	},
-	Type: function.StaticReturnType(cty.Number),
+	Type:         function.StaticReturnType(cty.Number),
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		if !(args[0].Type().IsListType() || args[0].Type().IsTupleType()) {
 			return cty.NilVal, errors.New("argument must be a list or tuple")
@@ -346,6 +351,7 @@ var MatchkeysFunc = function.New(&function.Spec{
 		// the return type is based on args[0] (values)
 		return args[0].Type(), nil
 	},
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		if !args[0].IsKnown() {
 			return cty.UnknownVal(cty.List(retType.ElementType())), nil
@@ -489,7 +495,8 @@ var SumFunc = function.New(&function.Spec{
 			Type: cty.DynamicPseudoType,
 		},
 	},
-	Type: function.StaticReturnType(cty.Number),
+	Type:         function.StaticReturnType(cty.Number),
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 
 		if !args[0].CanIterateElements() {
@@ -558,7 +565,8 @@ var TransposeFunc = function.New(&function.Spec{
 			Type: cty.Map(cty.List(cty.String)),
 		},
 	},
-	Type: function.StaticReturnType(cty.Map(cty.List(cty.String))),
+	Type:         function.StaticReturnType(cty.Map(cty.List(cty.String))),
+	RefineResult: refineNotNull,
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
 		inputMap := args[0]
 		if !inputMap.IsWhollyKnown() {

commit bc8e3808166804b4ee784abea48b826b56adb36d

fix: add missing tempdir declaration, regression introduced by f925124b1b2f3d7dac9aba78ef21558b8ed619bf

pkg/ci/stage_run.go
@@ -308,7 +308,6 @@ func (s *Stage) Run(ctx context.Context) diag.Diagnostics {
 			runArgs = append(runArgs, a.AsString())
 		}
 	} else if s.Container == nil {
-		fmt.Println(args.Type())
 		// if the container is not null, we may rely on internal args or entrypoint scripts
 		diags = diags.Append(diag.Diagnostic{
 			Severity: diag.SeverityError,

pkg/orchestra/context.go
@@ -2,7 +2,6 @@ package orchestra
 
 import (
 	"context"
-	"fmt"
 	"github.com/google/uuid"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/ext/tryfunc"
@@ -59,7 +58,6 @@ func NewContextWithTogomak(cfg Config) (Togomak, context.Context) {
 	// --> set up the working directory
 	cwd := Chdir(cfg, logger)
 	x.Must(err)
-	fmt.Println(cwd)
 
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, c.TogomakContextTempDir, tmpDir)
@@ -253,7 +251,7 @@ func NewContextWithTogomak(cfg Config) (Togomak, context.Context) {
 		hclDiagWriter: diagnosticTextWriter,
 		parser:        parser,
 		ectx:          hclContext,
-		tempDir:       "",
+		tempDir:       tmpDir,
 	}
 
 	return t, ctx

pkg/pipeline/parse.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/srevinsaju/togomak/v1/pkg/c"
 	"github.com/srevinsaju/togomak/v1/pkg/ci"
+	"github.com/srevinsaju/togomak/v1/pkg/meta"
 	"path/filepath"
 )
 
@@ -14,7 +15,7 @@ func Read(ctx context.Context, parser *hclparse.Parser) (*ci.Pipeline, hcl.Diagn
 
 	filePath := ctx.Value(c.TogomakContextPipelineFilePath).(string)
 	if filePath == "" {
-		filePath = "togomak.hcl"
+		filePath = meta.ConfigFileName
 	}
 	owd := ctx.Value(c.TogomakContextOwd).(string)
 