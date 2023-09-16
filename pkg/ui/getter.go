6fd751c642939450ab2f49334698a85bfc2b74e3
Srevin Saju <srevinsaju@sugarlabs.org>
2023-09-16 22:01:11 +0300

    feat(progress): add a passive progress bar

---
+++ pkg/ui/getter.go
@@ -0,0 +1,39 @@
+package ui
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/srevinsaju/togomak/v1/pkg/x"
+	"io"
+)
+
+// GetterProgressBar is a progress bar implementation for terraform downloads
+type GetterProgressBar struct {
+	Logger *logrus.Entry
+	src    string
+	pb     *ProgressWriter
+}
+
+func NewGetterProgressBar(logger *logrus.Entry, src string) *GetterProgressBar {
+	return &GetterProgressBar{
+		Logger: logger,
+		src:    src,
+	}
+}
+
+// Init initializes the progress bar
+func (e *GetterProgressBar) Init() *GetterProgressBar {
+	e.pb = NewProgressWriter(e.Logger, fmt.Sprintf("downloading %s", e.src))
+	return e
+}
+
+// TrackProgress tracks the progress of the download using the default ui.ProgressWriter implementation
+func (e *GetterProgressBar) TrackProgress(src string, currentSize, totalSize int64, stream io.ReadCloser) (body io.ReadCloser) {
+	for {
+		_, err := io.CopyN(e.pb, stream, 1)
+		if err != nil {
+			x.Must(e.pb.Close())
+			return stream
+		}
+	}
+}
