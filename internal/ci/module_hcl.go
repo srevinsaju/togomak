b9d9b0a88345009d301077ab87159711a18c0b58
Srevin Saju <srevinsaju@sugarlabs.org>
2023-11-17 20:05:53 +0300

    fix: depends_on ignored for modules

--- internal/ci/module_hcl.go
+++ internal/ci/module_hcl.go
@@ -3,5 +3,10 @@ package ci
 import "github.com/hashicorp/hcl/v2"
 
 func (m *Module) Variables() []hcl.Traversal {
-	return m.Source.Variables()
+	var vars []hcl.Traversal
+	vars = append(vars, m.Source.Variables()...)
+	vars = append(vars, m.DependsOn.Variables()...)
+	vars = append(vars, m.Condition.Variables()...)
+	vars = append(vars, m.ForEach.Variables()...)
+	return vars
 }
